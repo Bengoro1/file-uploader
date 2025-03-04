9587ef28ee30c0c8ecfecc232c9062aee0d05385
Bengoro <127534872+Bengoro1@users.noreply.github.com>
2025-03-04 13:56:57 +0100

    set express app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const exp = require('node:constants');
+const path = require('node:path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
