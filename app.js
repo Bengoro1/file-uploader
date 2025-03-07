b8f4adde22a8075a923e79812f57c3132b63abf8
Bengoro <127534872+Bengoro1@users.noreply.github.com>
2025-03-07 19:59:47 +0100

    convert from CommonJS to ESM

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('node:path');
+import express from 'express';
+import path from 'node:path';
 
 const app = express();
 
--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,1 +1,10 @@
-const passport = require()
+import passport from 'passport';
+import {Strategy as LocalStrategy} from'passport-local';
+import * as bcrypt from 'bcryptjs';
+import prisma from '../db/prisma';
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+
+  }
+}))
